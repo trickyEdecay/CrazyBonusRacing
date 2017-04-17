52b5d618d09c621a497e03a56166455a8d8925d4
trickyEdecay <517672932@qq.com>
2017-04-18 03:22:16 +0800

    修改配置文件 初始化npm

--- inc/init.inc.php
+++ inc/init.inc.php
@@ -1,16 +1,50 @@
 <?php
 
+
+
+#后台地址前缀
+define("ADMIN_PREFIX","/cbradmin");
+
+
 #根目录地址
-define("ROOT",__DIR__);
+define("ROOT",dirname(dirname(__FILE__)));
 
 #php 插件目录
-define("VENDOR","vendor");
+define("VENDOR","/vendor");
 
+#页面目录
+define("PAGES","/pages");
 
+#config 目录
+define("CONFIG","/config");
 
-set_include_path(get_include_path().PATH_SEPARATOR.VENDOR);
+#读取 config.json
+$config_json = json_decode(file_get_contents(ROOT.CONFIG."/config.json"),true);
 
+#网站目录前缀
+define("ROOT_PREFIX","/".$config_json["website_prefix"]);
+
+#添加插件目录到搜索变量中
+set_include_path(get_include_path().PATH_SEPARATOR.ROOT.VENDOR);
+
+#注册自动加载类的方法，新建对象的时候会调用这个函数
 spl_autoload_register(function ($class_name) {
     $class_name= str_replace("\\","/",$class_name).".php";
     require_once($class_name);
 });
+
+
+
+#数据库相关信息
+
+#数据库地址
+define("DB_HOST",$config_json["db_host"]);
+
+#数据库用户
+define("DB_USR",$config_json["db_usr"]);
+
+#数据库密码
+define("DB_PWD",$config_json["db_pwd"]);
+
+#所使用的数据库名
+define("DB_TABLE",$config_json["db_table"]);
--- index.php
+++ index.php
@@ -3,14 +3,18 @@
 require_once ("inc/init.inc.php");
 
 
+#使用Klein 来作为路由
 $klein = new \Klein\Klein();
 
-$klein->respond('GET', '/test/helloworld', function () {
-    die( 'Hello World!');
+# cbradmin/question
+$klein->respond('GET', ROOT_PREFIX.ADMIN_PREFIX."/question", function () {
+    require_once (PAGES."/admin/question.php");
+    die();
 });
 
+# not found
 $klein->respond(function ($re) {
-    return 'All the things'.$re->uri();
+    die('could not found this uri: '.$re->uri());
 });
 
 $klein->dispatch();
