2c4a4f91227a0c2fb76096604fa423d2bdf8cc97
trickyEdecay <517672932@qq.com>
2017-04-24 10:58:40 +0800

    新增用户页面路由

--- index.php
+++ index.php
@@ -6,6 +6,13 @@ require_once ("inc/init.inc.php");
 #使用Klein 来作为路由
 $klein = new \Klein\Klein();
 
+
+# 用户页面
+$klein->respond('GET', ROOT_PREFIX."/[:action]", function ($request) {
+    require_once (PAGES."/user/".$request->action.".php");
+    die();
+});
+
 # cbradmin/question
 $klein->respond('GET', ROOT_PREFIX.ADMIN_PREFIX."/[:action]", function ($request) {
     require_once (PAGES."/admin/".$request->action.".php");
